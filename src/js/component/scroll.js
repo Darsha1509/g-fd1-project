9704ca91515f34a4967435df0fa349dd7816c9f9
darya <kokorina.darya1509@gmail.com>
2018-12-22 15:11:07 +0300

    changes in scroll.js

--- src/js/component/scroll.js
+++ src/js/component/scroll.js
@@ -21,11 +21,7 @@
             }, 700);
     });
 
-    document.querySelector('.places .destination-button').addEventListener('click', function() {
-        $('html').animate({
-                scrollTop: $('#destination').offset().top
-            }, 700);
-    });
+    //на "need more places button"
 
     document.querySelector('.location .destination-button').addEventListener('click', function() {
         $('html').animate({
