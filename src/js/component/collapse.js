403cb4c54fd09c0d6ea34a21d4ca586b0eecad60
darya <kokorina.darya1509@gmail.com>
2018-12-22 16:30:50 +0300

    collapse.js cosmetic commit

--- src/js/component/collapse.js
+++ src/js/component/collapse.js
@@ -1,20 +1,16 @@
 document.querySelectorAll('.item').forEach(function(item, index, items) {
-      item.addEventListener('click', function() {
+    item.addEventListener('click', function() {
 
     var contents = document.querySelectorAll('.item-content');
     var currentElement = contents[index];
 
-    if (currentElement.classList.contains('item-content__opened')) {
-      currentElement.classList.toggle('item-content__opened',false);
+    if(currentElement.classList.contains('item-content__opened')) {
+        currentElement.classList.toggle('item-content__opened', false);
     } else {
-       
-       contents.forEach(function(content) {
-      
-        content.classList.toggle('item-content__opened', false);
-      });
-
-       currentElement.classList.toggle('item-content__opened');
+            contents.forEach(function(content) {
+            content.classList.toggle('item-content__opened', false);
+        });
+        currentElement.classList.toggle('item-content__opened');
     }
-
-  });
+    });
 });
